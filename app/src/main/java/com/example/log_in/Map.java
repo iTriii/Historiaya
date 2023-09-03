c51a2d2d153e3778a1018a12cf05a26c4147a8dd
Eunice <ordeunice10@gmail.com>
2023-09-03 21:17:39 +0800

    1st

--- app/src/main/java/com/example/log_in/Map.java
+++ app/src/main/java/com/example/log_in/Map.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Map extends AppCompatActivity {
-ImageButton backbutt;
+    ImageButton backbutt;
+    ConstraintLayout doncat1, gala1, doncatExpanded, galaExpanded;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,13 +18,31 @@ ImageButton backbutt;
 
         backbutt = findViewById(R.id.backbutt);
         backbutt.setOnClickListener(v -> Main2());
+
+        doncat1 = findViewById(R.id.doncat1);
+        gala1 = findViewById(R.id.gala1);
+        doncatExpanded = findViewById(R.id.doncatExpanded);
+        galaExpanded = findViewById(R.id.galaExpanded);
+
+        doncat1.setOnClickListener(v -> toggleExpandedViews(doncatExpanded, galaExpanded));
+        gala1.setOnClickListener(v -> toggleExpandedViews(galaExpanded, doncatExpanded));
     }
+
     public void Main2() {
         Intent intent = new Intent(this, Main2.class);
         startActivity(intent);
         overridePendingTransition(com.blogspot.atifsoftwares.animatoolib.R.anim.animate_slide_in_left, com.blogspot.atifsoftwares.animatoolib.R.anim.animate_slide_out_right);
     }
 
-    public void expand(View view) {
+    public void toggleExpandedViews(View viewToExpand, View viewToCollapse) {
+        if (viewToCollapse.getVisibility() == View.VISIBLE) {
+            viewToCollapse.setVisibility(View.GONE);
+        }
+
+        if (viewToExpand.getVisibility() == View.VISIBLE) {
+            viewToExpand.setVisibility(View.GONE);
+        } else {
+            viewToExpand.setVisibility(View.VISIBLE);
+        }
     }
 }
