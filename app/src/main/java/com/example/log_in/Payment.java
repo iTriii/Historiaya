384f3fa6d65d8240f0ce5da6adcda17472b78115
Leah Sulongsawa <91388847+Leilunaaa@users.noreply.github.com>
2023-11-08 11:28:33 +0800

    / payment details. / toasting the spinner / connected to firestore / tf booking calendar/time.summary of booking

--- app/src/main/java/com/example/log_in/Payment.java
+++ app/src/main/java/com/example/log_in/Payment.java
@@ -1,21 +1,153 @@
 package com.example.log_in;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.example.log_in.databinding.ActivityPaymentBinding;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class Payment extends AppCompatActivity {
-    private ActivityPaymentBinding binding;
+    Button btndone;
+    ImageButton uploadbtn, selectbtn; // Change to ImageButton
+    StorageReference storageRef;
+    LinearProgressIndicator progress;
+    ImageView imageView;
+    Toolbar supportActionBar;
+    FirebaseFirestore db;
+    private Uri selectedImageUri;
+    private String imageUrl = "";
+    int SELECT_PICTURE = 1;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityPaymentBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-    }
-    private void setListener(){
+        setContentView(R.layout.activity_payment);
 
+        FirebaseApp.initializeApp(this);
+        storageRef = FirebaseStorage.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        supportActionBar = findViewById(R.id.toolbar);
+        progress = findViewById(R.id.progress);
+        imageView = findViewById(R.id.imgviewpayment);
+        btndone = findViewById(R.id.okbtn);
+        selectbtn = findViewById(R.id.select);
+
+
+        // Add a click listener for the select button
+        selectbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Start an activity to select an image
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, SELECT_PICTURE);
+            }
+        });
+
+        // Add a click listener for the "Done" button
+        btndone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check if an image has been selected
+                if (selectedImageUri == null) {
+                    Toast.makeText(Payment.this, "Please select an image", Toast.LENGTH_LONG).show();
+                } else {
+                    // Upload the selected image to Firebase Storage
+                    uploadImageToFirebaseStorage(selectedImageUri);
+                }
+            }
+        });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                selectedImageUri = data.getData();
+                if (selectedImageUri != null) {
+                    // Apply Glide to load and crop the image
+                    RequestOptions requestOptions = new RequestOptions();
+                    Glide.with(Payment.this)
+                            .load(selectedImageUri)
+                            .apply(requestOptions)
+                            .into(imageView);
+                }
+            }
+        }
+    }
+
+    private void uploadImageToFirebaseStorage(Uri imageUri) {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("proofOfPayment");
+        String imageName = "image_" + System.currentTimeMillis() + ".jpg";
+        StorageReference imageRef = storageRef.child(imageName);
+
+        imageRef.putFile(imageUri)
+                .addOnSuccessListener(taskSnapshot -> {
+                    imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                        String imageUrl = uri.toString();
+                        saveUserDataToFirestore(mAuth.getCurrentUser(), imageUrl);
+                    });
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Image upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
+        progress.setVisibility(View.VISIBLE);
+    }
+
+    private void saveUserDataToFirestore(FirebaseUser user, String imageUrl) {
+        if (user != null) {
+            DocumentReference userDocRef = db.collection("users").document(user.getUid()).getFirestore().document("proof");
+            Map<String, Object> userData = new HashMap<>();
+            userData.put("ImageUrl", imageUrl);
+
+            // Add a Log statement to check the imageUrl
+            Log.d(TAG, "Image URL: " + imageUrl);
+
+            userDocRef.set(userData)
+                    .addOnCompleteListener(task -> {
+                        if (!task.isSuccessful()) {
+                            Log.e(TAG, "Failed to save user data to Firestore: " + task.getException().getMessage());
+                        }
+                    });
+        }
+    }
 }
--- app/src/main/java/com/example/log_in/fragmentDonCata.java
+++ app/src/main/java/com/example/log_in/fragmentDonCata.java
@@ -10,19 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragmentDonCata#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class fragmentDonCata extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +22,6 @@ public class fragmentDonCata extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragmentDonCata.
-     */
-    // TODO: Rename and change types and number of parameters
     public static fragmentDonCata newInstance(String param1, String param2) {
         fragmentDonCata fragment = new fragmentDonCata();
         Bundle args = new Bundle();
