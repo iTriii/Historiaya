567c2721e581abeb66229807fa3d8254b4c650a4
Leah Sulongsawa <91388847+Leilunaaa@users.noreply.github.com>
2023-11-23 14:14:26 +0800

    //Receptionist, Log in ng mga admins

---
+++ app/src/main/java/com/example/log_in/Receptionist_Adapter_History.java
@@ -0,0 +1,69 @@
+package com.example.log_in;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class Receptionist_Adapter_History extends RecyclerView.Adapter<Receptionist_Adapter_History .MyViewHolder> {
+    Context context;
+    ArrayList<User> userArrayList;
+    private FirebaseFirestore db;
+
+    public Receptionist_Adapter_History (Context context, ArrayList<User> userArrayList, FirebaseFirestore firestore) {
+        this.context = context;
+        this.userArrayList = userArrayList;
+        this.db = firestore;
+    }
+
+
+    @NonNull
+    @Override
+    public Receptionist_Adapter_History.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.receptionist_history_item, parent, false);
+        return new Receptionist_Adapter_History.MyViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull Receptionist_Adapter_History.MyViewHolder holder, int position) {
+        User HistoryUsers = userArrayList.get(position);
+
+        holder.MontHistoRecepText.setText(HistoryUsers.getReservedDate());
+        holder.BahayHistoRecepText.setText(HistoryUsers.getSelectedTour());
+        holder.ArawHistoRecepText.setText(HistoryUsers.getReservedDate());
+        holder.bookebyNameReceptionist_History.setText(HistoryUsers.getEmail());
+        holder.TotalNumberReceptionist_History.setText(HistoryUsers.getSelectedTouristNum());
+        holder.SelectedHouseReceptionist_History.setText(HistoryUsers.getSelectedTour());
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return userArrayList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView MontHistoRecepText, BahayHistoRecepText, ArawHistoRecepText,bookebyNameReceptionist_History,TotalNumberReceptionist_History,SelectedHouseReceptionist_History;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            MontHistoRecepText = itemView.findViewById(R.id.MontHistoRecepText);
+            BahayHistoRecepText = itemView.findViewById(R.id.BahayHistoRecepText);
+            ArawHistoRecepText = itemView.findViewById(R.id.ArawHistoRecepText);
+            bookebyNameReceptionist_History = itemView.findViewById(R.id.bookebyNameReceptionist_History);
+            TotalNumberReceptionist_History = itemView.findViewById(R.id.TotalNumberReceptionist_History);
+            SelectedHouseReceptionist_History = itemView.findViewById(R.id.SelectedHouseReceptionist_History);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/log_in/Receptionist_Upcoming_Adapter.java
@@ -0,0 +1,63 @@
+package com.example.log_in;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class Receptionist_Upcoming_Adapter extends RecyclerView.Adapter< Receptionist_Upcoming_Adapter.MyViewHolder> {
+    Context context;
+    ArrayList<User> userArrayList;
+    private FirebaseFirestore db;
+
+    public  Receptionist_Upcoming_Adapter(Context context, ArrayList<User> userArrayList, FirebaseFirestore firestore) {
+        this.context = context;
+        this.userArrayList = userArrayList;
+        this.db = firestore;
+    }
+
+
+    @NonNull
+    @Override
+    public Receptionist_Upcoming_Adapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.receptionist_upcoming_item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull Receptionist_Upcoming_Adapter.MyViewHolder holder, int position) {
+        User UpcomingUsers_Recep = userArrayList.get(position);
+
+        holder.MonthTextRecep_Upcoming.setText(UpcomingUsers_Recep.getReservedDate());
+        holder.BahayRecepUpcoming_Text.setText(UpcomingUsers_Recep.getSelectedTour());
+        holder.ArawRecepUpcoming_Text.setText(UpcomingUsers_Recep.getReservedDate());
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return userArrayList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView MonthTextRecep_Upcoming, BahayRecepUpcoming_Text, ArawRecepUpcoming_Text;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            MonthTextRecep_Upcoming = itemView.findViewById(R.id.MonthTextRecep_Upcoming);
+            BahayRecepUpcoming_Text = itemView.findViewById(R.id.BahayRecepUpcoming_Text);
+            ArawRecepUpcoming_Text = itemView.findViewById(R.id.ArawRecepUpcoming_Text);
+
+        }
+    }
+}
