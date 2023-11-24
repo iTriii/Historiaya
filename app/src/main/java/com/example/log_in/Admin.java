324d9e52876737e483ad8d1cf0e29b1f7f0cadd7
Eunice <ordeunice10@gmail.com>
2023-11-24 12:58:34 +0800

    1st

--- app/src/main/java/com/example/log_in/Admin.java
+++ app/src/main/java/com/example/log_in/Admin.java
@@ -3,11 +3,11 @@ package com.example.log_in;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Admin extends AppCompatActivity {
 
+    // Declaring TextView variables
     TextView StoreManager, DonCatbtn, Galarodbtn, Recep, TourismHead;
 
     @Override
@@ -15,39 +15,42 @@ public class Admin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        // Initializing TextView variables by finding their respective views
         StoreManager = findViewById(R.id.StoreManager);
-
         DonCatbtn = findViewById(R.id.DonCatbtn);
         Galarodbtn = findViewById(R.id.Galarodbtn);
         Recep = findViewById(R.id.Recep);
         TourismHead = findViewById(R.id.TourismHead);
 
-//RECEPTIONIST
+        // Click listener for Receptionist TextView
         Recep.setOnClickListener(v -> {
+            // Creating intent to navigate to Receptionist class
             Intent intent = new Intent(Admin.this, Receptionist.class);
             startActivity(intent);
         });
-//NAVIGATE TO DON CATALINO ADIN ACTIVITY
-        DonCatbtn .setOnClickListener(v -> {
+
+        // Click listener to navigate to DonCatalinoAdmin activity
+        DonCatbtn.setOnClickListener(v -> {
             Intent intent = new Intent(Admin.this, DonCatalinoAdmin.class);
             startActivity(intent);
         });
-        // NAVIGATE TO GALA RODRIGUEZ ADMIN ACTIVITY
-        Galarodbtn .setOnClickListener(v -> {
+
+        // Click listener to navigate to GalaRodriguezAdmin activity
+        Galarodbtn.setOnClickListener(v -> {
             Intent intent = new Intent(Admin.this, GalaRodriguezAdmin.class);
             startActivity(intent);
         });
-        // NAVIGATE TO TOURISMHEAD ADMIN ACTIVITY
+
+        // Click listener to navigate to TourismHeadAdmin activity
         TourismHead.setOnClickListener(v -> {
             Intent intent = new Intent(Admin.this, TourismHeadAdmin.class);
             startActivity(intent);
         });
-        // NAVIGATE TO STORE MANAGER
+
+        // Click listener to navigate to StoreManager activity
         StoreManager.setOnClickListener(v -> {
             Intent intent = new Intent(Admin.this, StoreManager.class);
             startActivity(intent);
         });
-
     }
-
 }
