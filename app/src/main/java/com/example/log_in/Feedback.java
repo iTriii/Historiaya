0c12ef14bc4a20c6b0fb390e7d1a62397855cfe6
Leah Sulongsawa <91388847+Leilunaaa@users.noreply.github.com>
2023-11-27 19:01:13 +0800

    feedback done/ firestore done

--- app/src/main/java/com/example/log_in/Feedback.java
+++ app/src/main/java/com/example/log_in/Feedback.java
@@ -2,6 +2,7 @@ package com.example.log_in;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -19,7 +22,7 @@ import java.util.Map;
 public class Feedback extends AppCompatActivity {
     private EditText feedbackEditText;
     private Button submitButton;
-    ImageButton backbtn,exitbtn;
+    ImageButton backbtn, exitbtn;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private String userId;
@@ -31,9 +34,8 @@ public class Feedback extends AppCompatActivity {
 
         db = FirebaseFirestore.getInstance();
         mAuth = FirebaseAuth.getInstance();
-        backbtn= findViewById(R.id.backbtn);
+        backbtn = findViewById(R.id.backbtn);
         exitbtn = findViewById(R.id.exitbtn);
-
 
         feedbackEditText = findViewById(R.id.editTextFeedback);
         submitButton = findViewById(R.id.Submitbtn);
@@ -74,20 +76,37 @@ public class Feedback extends AppCompatActivity {
     }
 
     private void saveFeedbackToFirestore(String feedback) {
-        // Create a new feedback document
-        Map<String, Object> feedbackData = new HashMap<>();
-        feedbackData.put("feedback", feedback);
+        if (mAuth.getCurrentUser() != null) {
+            String userId = mAuth.getCurrentUser().getUid();
 
-        // Add the document to the "feedback" collection using the user's UID
-        db.collection("feedback")
-                .add(feedbackData)
-                .addOnSuccessListener(documentReference -> {
-                    // Feedback saved successfully
-                    Toast.makeText(this, "Feedback submitted", Toast.LENGTH_SHORT).show();
-                })
-                .addOnFailureListener(e -> {
-                    // Handle failure
-                    Toast.makeText(this, "Failed to submit feedback", Toast.LENGTH_SHORT).show();
-                });
+            // Initialize Firestore references
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference userDocRef = db.collection("users").document(userId);
+            CollectionReference feedbackCollectionRef = userDocRef.collection("Feedback");
+
+            // Create a new document in the "Feedback" collection
+            DocumentReference newFeedbackDocRef = feedbackCollectionRef.document();
+
+            // Create a new feedback document
+            Map<String, Object> userFeedback = new HashMap<>();
+            userFeedback.put("userFeedback", feedback);
+
+            // Add the document to the "Feedback" collection
+            newFeedbackDocRef.set(userFeedback)
+                    .addOnSuccessListener(documentReference -> {
+                        // Feedback saved successfully
+                        Toast.makeText(this, "Feedback submitted", Toast.LENGTH_SHORT).show();
+                        Log.d("Feedback", "Feedback submitted successfully");
+                    })
+                    .addOnFailureListener(e -> {
+                        // Handle failure
+                        Toast.makeText(this, "Failed to submit feedback", Toast.LENGTH_SHORT).show();
+                        Log.e("Feedback", "Error submitting feedback", e);
+                    });
+        } else {
+            // Log an error if the current user is null
+            Log.e("Feedback", "Current user is null");
+            Toast.makeText(this, "Failed to submit feedback. Please log in.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
